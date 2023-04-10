d5b57de96ff844f1941b49064801c0ea3eb426f8
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-04-10 16:14:32 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
